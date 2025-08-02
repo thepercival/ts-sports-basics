0b04e3c925b3081e71d5f7fc960101140b49a3e6
thepercival <coendunnink@gmail.com>
2025-08-02 10:32:55 +0200

    fix(scope): add

--- src/selfRefereeScope.ts
+++ src/selfRefereeScope.ts
@@ -1,4 +1,5 @@
 export enum SelfRefereeScope {
     SamePoule = "SamePoule",
-    OtherPoules = "OtherPoules"
+    OtherPoulesSameCategory = "OtherPoulesSameCategory",
+    OtherPoulesOtherCategories = "OtherPoulesOtherCategories"
 }
